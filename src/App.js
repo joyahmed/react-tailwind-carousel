b3c2ab63122f03180e1e4f77c179a7ad6bc95d4f
joyahmed <joy_ahmed_007@yahoo.com>
2022-09-21 19:44:37 +0600

    !✅

--- src/App.js
+++ src/App.js
@@ -1,25 +1,192 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { data } from './cosnstants';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [mounted, setMounted] = useState(false);
+	const [slider, setSlider] = useState();
+	const [autoStart] = useState(false);
+	const [current, setCurrent] = useState(0);
+	const [imageSize, setImageSize] = useState('');
+	const [ sliderWidth, setSliderWidth ] = useState('');
+	const [sliderItems, setSliderItems] = useState(data)
+	const sliderRef = useRef(null);
+	const leftButton = useRef();
+	const rightButton = useRef();
+
+	useEffect(() => {
+		const handleMount = setMounted(true);
+		return () => handleMount;
+	}, []);
+
+	useEffect(() => {
+		const handleSliderWidth =
+			mounted && setSliderWidth(sliderRef?.current?.offsetWidth);
+
+		return () => handleSliderWidth;
+	}, [mounted]);
+
+	const changeWidth = () => {
+		setSliderWidth(sliderRef?.current?.offsetWidth);
+	};
+
+	useEffect(() => {
+		window.addEventListener('resize', () => changeWidth());
+		return () => window.removeEventListener('resize', changeWidth());
+	}, []);
+
+	useEffect(() => {
+		const handleImageSize = () => {
+			if (sliderWidth < 500) setImageSize(sliderWidth);
+			else if (sliderWidth > 500 && sliderWidth < 1000)
+				setImageSize(sliderWidth / 2);
+			else if (sliderWidth > 1000 && sliderWidth < 1300)
+				setImageSize(sliderWidth / 3);
+			else setImageSize(sliderWidth / 3);
+		};
+		handleImageSize();
+		return () => handleImageSize();
+	}, [sliderWidth]);
+
+	useEffect(() => {
+		const handleSlider = mounted && setSlider(sliderRef.current);
+		return () => handleSlider;
+	}, [mounted]);
+
+	let slideInterval;
+	let intervalTime = 3000;
+
+	const start = () => {
+		moveRight();
+		moveSlide();
+	};
+
+	const autoSlide = () => {
+		moveSlide();
+		slideInterval = setInterval(start, intervalTime);
+	};
+
+	useEffect(() => {
+		autoStart && autoSlide();
+		return () => clearInterval(slideInterval);
+		//eslint-disable-next-line
+	}, [current]);
+
+	const moveSlide = () => {
+		current >= data.length / 3 - 1 && setCurrent(0);
+
+		if (slider)
+			slider.style.transform = `translateX(${
+				(-current * sliderWidth) / 3
+			}px)`;
+	};
+
+	const moveRight = () => {
+		current < data.length / 2
+			? setCurrent(current + 1)
+			: setCurrent(0);
+	};
+
+	const handleNavigation = direction => {
+		if (direction === 'right') {
+			moveRight();
+		} else {
+			if (current > 0) {
+				setCurrent(current - 1);
+			} else if (current === 0) {
+				setCurrent(data.length / 3 - 1);
+			}
+		}
+		moveSlide();
+	};
+
+	if (!mounted) {
+		return (
+			<div className='flex items-center justify-center h-screen w-screen'>
+				Loading..
+			</div>
+		);
+	}
+
+	return (
+		<div className='flex items-center justify-center w-screen h-screen bg-gradient-to-b from-black via-blue-900 to-black text-white'>
+			<div className='relative overflow-hidden h-screen w-screen bg-transparent'>
+				<div
+					ref={sliderRef}
+					className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-center transition-all duration-[0.5s] ease-in-out bg-blend-multiply shadow-xl container'
+				>
+					{data.map(item => (
+						<img
+							key={item.id}
+							src={item.source}
+							width={`${imageSize}px`}
+							height={`${imageSize}px`}
+							alt=''
+							className='h-[15rem] sm:h-[17rem] object-cover z-0'
+						/>
+					))}
+				</div>
+				<div className='absolute top-0 bottom-0 left-0 right-0 flex items-center justify-between'>
+					<Button
+						ref={leftButton}
+						onClick={() => handleNavigation('left')}
+						icon={<ChevronLeft />}
+					/>
+
+					<Button
+						ref={rightButton}
+						onClick={() => handleNavigation('right')}
+						icon={<ChevronRight />}
+					/>
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default App;
+
+const Button = ({ onClick, icon }) => {
+	return (
+		<button
+			type='button'
+			className='bg-[white]/20 px-4 py-2.5 opacity-50 hover:opacity-100 text-lg font-bold h-full shadow-xl'
+			onClick={onClick}
+		>
+			{icon}
+		</button>
+	);
+};
+
+const ChevronLeft = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={2}
+		stroke='currentColor'
+		className='w-6 h-6 sm:w-9 sm:h-9'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='M15.75 19.5L8.25 12l7.5-7.5'
+		/>
+	</svg>
+);
+
+const ChevronRight = () => (
+	<svg
+		xmlns='http://www.w3.org/2000/svg'
+		fill='none'
+		viewBox='0 0 24 24'
+		strokeWidth={2}
+		stroke='currentColor'
+		className='w-6 h-6 sm:w-9 sm:h-9'
+	>
+		<path
+			strokeLinecap='round'
+			strokeLinejoin='round'
+			d='M8.25 4.5l7.5 7.5-7.5 7.5'
+		/>
+	</svg>
+);
---
+++ src/cosnstants/index.js
@@ -0,0 +1,47 @@
+export const data = [
+	{
+		id: 1,
+		source:
+			'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80'
+	},
+	{
+		id: 2,
+		source:
+			'https://images.unsplash.com/photo-1480497490787-505ec076689f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+	},
+	{
+		id: 3,
+		source:
+			'https://images.unsplash.com/photo-1502528230654-e2161eb9f08a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
+	},
+	{
+		id: 4,
+		source:
+			'https://images.unsplash.com/photo-1544084944-15269ec7b5a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTQ0fHxuYXR1cmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60'
+	},
+	{
+		id: 5,
+		source:
+			'https://images.unsplash.com/photo-1469474968028-56623f02e42e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=874&q=80'
+	},
+	{
+		id: 6,
+		source:
+			'https://cdn.pixabay.com/photo/2015/06/19/21/24/avenue-815297__340.jpg'
+	},
+	{
+		id: 7,
+		source:
+			'https://cdn.pixabay.com/photo/2013/07/25/01/31/forest-166733__340.jpg'
+	},
+	{
+		id: 8,
+		source:
+			'https://images.pexels.com/photos/1062249/pexels-photo-1062249.jpeg?auto=compress&cs=tinysrgb&w=600'
+	},
+	{
+		id: 9,
+		source:
+			'https://images.unsplash.com/photo-1586521995568-39abaa0c2311?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlJTIwYmFja2dyb3VuZHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+	}
+];
